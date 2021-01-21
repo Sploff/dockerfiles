ce4a1e4be5fcac69b2fc78f91e820004091184fd
Christian Nilsson <christian.nilsson4@ikea.com>
2021-01-21 16:45:58 +0100

    added image to haCore

--- homeAssistantCore.sh
+++ homeAssistantCore.sh
@@ -1,8 +1,9 @@
 docker run \
 -d \
 -it \
--e TZ=Europe/Stockholm
--v /home/sploff/persistantDocker/homeAssistantCore:/config
---network host
---name homeAssistantCore
---restart unless-stopped
+-e TZ=Europe/Stockholm \
+-v /home/sploff/persistantDocker/homeAssistantCore:/config \
+--network host \
+--name homeAssistantCore \
+--restart unless-stopped \
+homeassistant/home-assistant:stable

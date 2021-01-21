38df8ff971353173587804f95e150d29687264d2
Christian Nilsson <christian.nilsson4@ikea.com>
2021-01-21 20:20:44 +0100

    runnable

--- node-red.sh
+++ node-red.sh
@@ -3,7 +3,7 @@ docker run \
 -it \
 -e TZ=Europe/Stockholm \
 -v /home/sploff/persistantDocker/node-red:/data \
--p 1880:1880
+-p 1880:1880 \
 --name node-red \
 --restart unless-stopped \
 nodered/node-red

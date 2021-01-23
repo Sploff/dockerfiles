e8c8194791673853d498eb43751d2334cb973d3d
Christian Nilsson <christian.nilsson4@ikea.com>
2021-01-23 20:33:15 +0100

    changed ttyUSB to ttyACM

--- deConz.sh
+++ deConz.sh
@@ -1,8 +1,8 @@
 docker run -d \
    -v /etc/localtime:/etc/localtime:ro \
    -v /home/sploff/persistantDocker/deConz:/root/.local/share/dresden-elektronik/deCONZ \
-   --name=deConz \
    --net=host \
-   --device=/dev/ttyUSB0 \
+   --device=/dev/ttyACM0 \
+   --name=deConz \
    --restart unless-stopped \
    marthoc/deconz

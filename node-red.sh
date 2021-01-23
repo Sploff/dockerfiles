bc4d69ddc572d58e4283b73afbee4142ead5d885
Christian Nilsson <christian.nilsson4@ikea.com>
2021-01-23 21:39:10 +0100

    network host

--- node-red.sh
+++ node-red.sh
@@ -3,7 +3,7 @@ docker run \
 -it \
 -e TZ=Europe/Stockholm \
 -v /home/sploff/persistantDocker/node-red:/data \
--p 1880:1880 \
+--network host \
 --name node-red \
 --restart unless-stopped \
 nodered/node-red

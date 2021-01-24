16768ed0923d73fcd57e4ad14a3a73014bfecb36
Christian Nilsson <christian.nilsson4@ikea.com>
2021-01-24 14:49:41 +0100

    enabled vnc

--- deConz.sh
+++ deConz.sh
@@ -1,6 +1,8 @@
 docker run -d \
    -v /etc/localtime:/etc/localtime:ro \
    -v /home/sploff/persistantDocker/deConz:/root/.local/share/dresden-elektronik/deCONZ \
+   -e DECONZ_VNC_MODE=1 \
+   -e DECONZ_VNC_PASSWORD=*****
    --net=host \
    --device=/dev/ttyACM0 \
    --name=deConz \

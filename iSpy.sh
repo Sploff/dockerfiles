ed9ba86462586a7b63e739c5fb59c55acb3fd88a
Christian Nilsson <christian.nilsson4@ikea.com>
2021-02-03 21:17:08 +0100

    Added volumes

--- iSpy.sh
+++ iSpy.sh
@@ -2,8 +2,11 @@ docker run \
 -d -it \
 -e TZ=Europe/Stockholm \
 -p 8090:8090 \
--p 3478:3478/udp \
+-p 3480:3478/udp \
 -p 50000-50010:50000-50010/udp \
---name agentdvr \
+-v ~/persistantDocker/iSpy/config/:/agent/Media/XML/ \
+-v ~/persistantDocker/iSpy/media/:/agent/Media/WebServerRoot/Media/ \
+-v ~/persistantDocker/iSpy/commands:/agent/Commands/ \
+--name iSpy \
 --restart unless-stopped \
 doitandbedone/ispyagentdvr:latest

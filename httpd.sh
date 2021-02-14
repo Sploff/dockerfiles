8c4d16586d4ac0f2168ffdec0d7f0da4566dc7ef
Christian Nilsson <christian.nilsson4@ikea.com>
2021-02-14 09:51:45 +0100

    changed httpd mount

--- httpd.sh
+++ httpd.sh
@@ -2,7 +2,7 @@ docker run \
 	-dit \
 	-e TZ=Europe/Stockholm \
 	-p 10100:80 \
-	-v /home/sploff/persistantDocker/apache-gopro:/usr/local/apache2/ \
+	-v /home/sploff/persistantDocker/apache-gopro/htdocs/:/usr/local/apache2/htdocs/ \
 	--name apache-gopro \
 	--restart unless-stopped \
 	httpd:2.4

f203258904817f0585a895944266ae13ca730857
Christian Nilsson <christian.nilsson4@ikea.com>
2021-02-16 22:33:42 +0100

    portainer

--- portainer.sh
+++ portainer.sh
@@ -4,7 +4,7 @@ docker run \
 	-p 10000:8000 \
 	-p 10001:9000 \
 	-v "/var/run/docker.sock:/var/run/docker.sock" \
-	-v /home/sploff/persistantDocker/portainer/data:/data
+	-v /home/sploff/persistantDocker/portainer/data:/data \
 	--name portainer \
 	--restart unless-stopped \
 	portainer/portainer-ce

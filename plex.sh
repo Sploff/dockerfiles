52c6b4e04c99a7599397508cc0f2daece16d810e
Christian Nilsson <christian.nilsson4@ikea.com>
2021-02-23 20:44:25 +0100

    portchange in plex

--- plex.sh
+++ plex.sh
@@ -4,17 +4,17 @@ docker run \
 	-e PGID=1000 \
 	-e TZ=Europe/Stockholm \
 	-e VERSION=docker \
-	#-e PLEX_CLAIM= `#optional` \
-	-p 32400:32400 \
-	-p 1900:1900/udp \
-	-p 3005:3005 \
-	-p 5353:5353/udp \
-	-p 8324:8324 \
-	-p 32410:32410/udp \
-	-p 32412:32412/udp \
-	-p 32413:32413/udp \
-	-p 32414:32414/udp \
-	-p 32469:32469 \
+	-e PLEX_CLAIM=`#optional` \
+	-p 10040:32400 \
+	-p 10041:1900/udp \
+	-p 10042:3005 \
+	-p 10043:5353/udp \
+	-p 10044:8324 \
+	-p 10045:32410/udp \
+	-p 10046:32412/udp \
+	-p 10047:32413/udp \
+	-p 10048:32414/udp \
+	-p 10049:32469 \
 	-v /home/sploff/persistantDocker/plex/library:/config \
 	-v /home/sploff/persistantDocker/plex/transcode:/transcode \
 	-v /home/sploff/persistantDocker/plex/media:/data \

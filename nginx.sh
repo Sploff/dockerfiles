67a27f2d5bebfcb7279455201aa08aa952699efa
Christian Nilsson <christian.nilsson4@ikea.com>
2021-03-06 08:46:07 +0100

    added volume

--- nginx.sh
+++ nginx.sh
@@ -2,7 +2,8 @@ docker run \
 -dit \
 -e TZ=Europe/Stockholm \
 -p 10200:80 \
--v /home/sploff/persistantDocker/nginx/:/usr/share/nginx/html \
+-v /home/sploff/persistantDocker/nginx/html:/usr/share/nginx/html \
+-v /home/sploff/persistantDocker/nginx/nginx.conf:/etc/nginx/nginx.conf:ro \
 --name nginx \
 --restart unless-stopped \
 nginx:stable
